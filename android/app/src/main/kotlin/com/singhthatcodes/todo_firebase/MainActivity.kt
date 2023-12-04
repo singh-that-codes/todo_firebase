e7343fac61e1db4d1d04c3fdd7df1bf93c8ddfa5
singh-that-codes <500091028@stu.upes.ac.in>
2023-12-04 23:33:20 +0530

    Project Started

---
+++ android/app/src/main/kotlin/com/singhthatcodes/todo_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.singhthatcodes.todo_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_firebase/auth/authscreen.dart';
+import 'package:todo_firebase/screens/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, usersnapshot){
+          if(usersnapshot.hasData){
+            return Home();
+          }
+          else{
+            return AuthScreen();
+          }
+        }
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark,primaryColor:Colors.purple),
+    );
+  }
+}
